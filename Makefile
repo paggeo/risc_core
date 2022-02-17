61418488e2537a0922aca7ffe0afdad8bcd0df20
GeorgePag4028 <georgepag4028@gmail.com>
2022-02-17 10:53:42 +0200

    Start add

--- Makefile
+++ Makefile
@@ -3,7 +3,6 @@ CFLAGS = -Wall -Wextra
 
 ## Remove some warnings.
 CFLAGS += -Wno-unused-parameter -Wno-unused-variable -Wno-unused-function
-
 # RES=-DPRINT_RESULTS
 
 all: float_point_unit
@@ -14,8 +13,10 @@ LL_FILE =
 ## Additional source files
 SRC_FILES = 
 
+#$(CC) $(CFLAGS) $^ -o $@
 float_point_unit: float_point_unit.c ${LL_FILE} ${SRC_FILES}
-	$(CC) $(CFLAGS) $^ -o $@
+	$(CC) $^ -o $@
+
 
 clean:
 	rm -f float_point_unit
--- float_point_unit.c
+++ float_point_unit.c
@@ -2,11 +2,29 @@
 /* https://www.h-schmidt.net/FloatConverter/IEEE754.html */
 /* Floating point multiplication */
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 
 #define S(a) (a>>31)
 #define E(a) ((a>>23)&((1<<8)-1))
 #define M(a) (a&((1<<23)-1))
+#define NormM(a) (a|(1<<23))
+#define bias  127
+
+void printBits(size_t const size, void const * const ptr)
+{
+    unsigned char *b = (unsigned char*) ptr;
+    unsigned char byte;
+    int i, j;
+    
+    for (i = size-1; i >= 0; i--) {
+        for (j = 7; j >= 0; j--) {
+            byte = (b[i] >> j) & 1;
+            printf("%u", byte);
+        }
+    }
+    puts("");
+}
 
 void fp_print(int a){
     printf("    Sign : %d\n",S(a));
@@ -15,6 +33,51 @@ void fp_print(int a){
 }
 
 int fp_add(int a, int b){
+  if (a == 0) return b;
+  if (b == 0) return a;
+  int sa, ea, ma , sb, eb, mb ;
+  sa = S(a);
+  ea = E(a) - bias;
+  ma = M(a);
+  sb = S(b);
+  eb = E(b) - bias;
+  /* mb = M(b)>>1 |(); */
+  // check how much shifting is required
+  int shifting = ea - eb;
+  printf("shifting : %d\n",shifting);
+  uint32_t ma_norm = NormM(ma);
+  uint32_t mb_norm = NormM(mb);
+
+  printBits(sizeof(ma),&ma);
+  printBits(sizeof(ma_norm),&ma_norm);
+  if (shifting == 0) {
+      int mc_norm = ma_norm + mb_norm;
+      int mc = M(mc_norm);
+      printf("    Sign : %d\n",0);
+      printf("Exponent : %d\n",E(a));
+      printf("Mantissa : %d\n",mc);
+  }
+  else if (shifting > 0) {
+      mb_norm = mb_norm >> shifting;
+      int mc_norm = ma_norm + mb_norm;
+      int mc = M(mc_norm);
+      printf("    Sign : %d\n",0);
+      printf("Exponent : %d\n",E(a));
+      printf("Mantissa : %d\n",mc);
+
+  }
+  else if (shifting < 0) {
+      ma_norm = ma_norm >>abs(shifting);
+      printf("%d\n",abs(shifting));
+      printBits(sizeof(ma_norm),&ma_norm);
+      int mc_norm = ma_norm + mb_norm;
+      printBits(sizeof(mc_norm),&mc_norm);
+      int mc = M(mc_norm);
+      printf("    Sign : %d\n",0);
+      printf("Exponent : %d\n",E(b));
+      printf("Mantissa : %d\n",mc);
+    
+  }
   return a+b;
 }
 
@@ -23,23 +86,17 @@ int fp_mult(int a,int b){
 }
 
 int main(){
-  float a = 1.3;
-  float b = 4.5;
+  float a = 1.6;
+  float b = 0.6;
   float c = a*b;
   
   int fa = *(uint32_t*)(&a);
   int fb = *(uint32_t*)(&b);
   int fc = *(uint64_t*)(&c);
-  printf("%f * %f = %f\n",a,b,c);
-  float d = a+b;
-  printf("%f + %f = %f\n",a,b,d);
-  printf("%x * %x = %x\n",fa,fb,fc);
-  fp_print(fa);
-  fp_print(fb);
-  fp_print(fc);
+  /* fp_print(fa); */
+  /* fp_print(fb); */
+  /* fp_print(fc); */
   fc = fp_add(fa,fb);
-  fp_print(fa);
-  fp_print(fb);
-  fp_print(fc);
+  /* printf("%d\n",fc); */
 }
 
