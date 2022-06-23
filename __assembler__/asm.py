99306950c3f7800a69a6f4eb745e8d883357cae9
GeorgePag4028 <georgepag4028@gmail.com>
2022-06-23 22:18:21 +0300

    assembler

--- __assembler__/asm.py
+++ __assembler__/asm.py
@@ -35,13 +35,29 @@ def replace(arr,start,string):
 	return arr
 	 
 def decode(instruction):
-	ins = str(instruction).replace(',','').replace('\n','').split(' ') 
+	ins = str(instruction).replace(',','').replace('\n','').replace('\t', '')
+	ins = ins.split(' ')
 	print(ins)
 	arr = ['0'] *32
+	if (ins[0] == 'lui' or ins[0] == 'auipc' ): ## U-type
+	 print('U-type')
+	elif (ins[0] == 'jal'):  ## J-type
+	 print('J-type')
+	elif (ins[0] == 'beq' or ins[0] == 'bne' or ins[0] == 'blt' or ins[0] == 'bge' or ins[0] == 'bltu' or ins[0] == 'bgeu'): ## B-type
+	 print('B-type')
+	elif (ins[0] == 'sb' or ins[0] == 'sh' or ins[0] == 'sw'): ## S-type 
+	 print('S-type')
+	elif (ins[0] == 'jalr' or ins[0] == 'lb' or ins[0] == 'lh' or ins[0] == 'lw' or ins[0] == 'lbu' or ins[0] == 'lhu' or ins[0] == 'addi' or ins[0] == 'slti' or ins[0] == 'sltiu' or ins[0] == 'xori' or ins[0] == 'ori' or ins[0] == 'andi'): ## I-type
+	 print('I-type')
+	elif (ins[0] == 'slli' or ins[0] == 'srli' or ins[0] == 'srai' or ins[0] == 'add' or ins[0] == 'sub' or ins[0] == 'sll' or ins[0] == 'slt' or ins[0] == 'sltu' or ins[0] == 'xor' or ins[0] == 'srl' or ins[0] == 'sra' or ins[0] == 'or' or ins[0] == 'and'): ## R-type
+	 print('R-type') 
+	
+
 	arr = replace(arr, 28, '1111')
 	return ''.join(str(e) for e in arr)
 	 
 if __name__ == '__main__' : 
+	print(regnames)
 	if not os.path.isdir('custom-isa-assembly'):
 		os.mkdir('custom-isa-assembly')
 	for x in glob.glob('custom-isa-tests/*'):
